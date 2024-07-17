fd1d10789d58c1c4d4c9e6d5d0d902bac661c9e1
muskankushwah1990 <muskankushwah1990@gmail.com>
2024-07-17 14:15:45 +0530

    newcommit

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,11 +1,10 @@
 const {error} = require('console');
 const mongoose = require('mongoose');
-// const local_URL = 'mongodb://127.0.0.1:27017/ResumeProject'
+ const local_URL = 'mongodb://127.0.0.1:27017/ResumeProject'
 
-const MONGO_ATLAS_URL = "mongodb+srv://Muskan:Muskan123@cluster0.2uxhbwt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = () => {
-    return mongoose.connect(MONGO_ATLAS_URL)
+    return mongoose.connect(local_URL)
     .then(() => {
         console.log('connect database successfully');
     }).catch((error) => {
